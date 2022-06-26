111185e58456462cb933260a50ae3952ca45ac57
IHateYourCode <89636625+IHateYourCode@users.noreply.github.com>
2022-06-26 17:26:58 -0400

    Documented API

--- Source/mod.ts
+++ Source/mod.ts
@@ -2,9 +2,28 @@
 import Split from './Split.js';
 
 
+/**
+ *  Split a string into it's perceived characters.
+ *  
+ *  ```javascript
+ *  import { split } from "https://deno.land/x/split@v0.0.2/mod.ts";
+ *  
+ *  split('🎩🧶🎮'); // [ '🎩' , '🧶' , '🎮' ]
+ */
+
 export function split( string : string ) : string [] {
     return Split(string);
 }
+
+
+/**
+ *  Count the number of perceived characters.
+ *
+ *  ```javascript
+ *  import { count } from "https://deno.land/x/split@v0.0.2/mod.ts";
+ *  
+ *  split('⛳Hi🏀'); // 4
+ */
 
 export function count( string : string ) : number {
     return Split(string).length;
